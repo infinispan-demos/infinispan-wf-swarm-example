f56bf29f27e6e5adbeceae840ed2d7e3a34173f7
Sebastian Laskawiec <slaskawi@redhat.com>
2016-10-03 14:38:25 +0200

    Final version

--- src/main/java/com/example/configuration/InfinispanConfiguration.java
+++ src/main/java/com/example/configuration/InfinispanConfiguration.java
@@ -1,18 +1,36 @@
 package com.example.configuration;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
 
 import org.infinispan.cdi.embedded.ConfigureCache;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 
 @ApplicationScoped
 public class InfinispanConfiguration {
 
    @ConfigureCache
+   @Produces
    @ApplicationScoped
    public Configuration configuration() {
-      return new ConfigurationBuilder().build();
+      return new ConfigurationBuilder()
+            .clustering()
+            .cacheMode(CacheMode.DIST_SYNC)
+            .build();
+   }
+
+   @Produces
+   @ApplicationScoped
+   public EmbeddedCacheManager cacheManager() {
+      GlobalConfigurationBuilder cacheManagerConfiguration = new GlobalConfigurationBuilder();
+      cacheManagerConfiguration.globalJmxStatistics().allowDuplicateDomains(true).enable();
+      cacheManagerConfiguration.transport().defaultTransport();
+      return new DefaultCacheManager(cacheManagerConfiguration.build());
    }
 
 }
--- src/main/java/com/example/rest/InfinispanEndpoint.java
+++ src/main/java/com/example/rest/InfinispanEndpoint.java
@@ -1,28 +1,29 @@
 package com.example.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import com.example.service.CacheService;
+import org.infinispan.Cache;
 
-@Path("/hello")
-public class HelloWorldEndpoint {
+@Path("/infinispan")
+public class InfinispanEndpoint {
 
-   @javax.inject.Inject
-   CacheService cacheService;
+   @Inject
+   Cache<String, String> cache;
 
    @GET
    @Path("cache")
-   public Response doGet() {
-      return Response.ok(cacheService.getContent()).build();
+   public Response printContent() {
+      return Response.ok(cache.keySet().toString()).build();
    }
 
    @POST
    @Path("cache")
    public Response addSomethingToTheCache(String text) {
-      cacheService.addToCache(text);
+      cache.put(text, text);
       return Response.created(null).build();
    }
 }
